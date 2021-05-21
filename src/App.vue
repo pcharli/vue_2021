50ea689f7b872310cd520e1dbe4e77e716a88b84
Pierre Charlier <pcepegra@gmail.com>
2021-05-21 11:51:39 +0200

    update titre home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home 2</router-link> |
     </div>
     <router-view/>
   </div>
